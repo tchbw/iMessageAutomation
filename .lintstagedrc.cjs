b4c3d77d7be25724e3d7f1603e883aa3d2fafdad
thomas <thomas.chang7@gmail.com>
2024-10-24 22:22:24 -0700

    Add pre-commit

---
+++ .lintstagedrc.cjs
@@ -0,0 +1,8 @@
+const path = require('path')
+
+const runTsc = () => `pnpm run typecheck` // --project ./tsconfig.react-jsx.json Function syntax allows us ignore files and run tsc on entire codebase
+module.exports = {
+  '*.{cjs,mjs,js,jsx,ts,tsx}': `eslint --fix`,
+  '*.ts*': runTsc,
+  '*': `prettier --list-different --ignore-unknown --write`
+}
